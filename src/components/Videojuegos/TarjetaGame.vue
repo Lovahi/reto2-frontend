3f6e7d11b4a6f60eb98e008baa5bf06e618f05a1
Younesse <younesse.khatibi@elorrieta-errekamari.com>
2026-01-22 20:41:20 +0100

    subida de tarjetas

--- src/components/Videojuegos/TarjetaGame.vue
+++ src/components/Videojuegos/TarjetaGame.vue
@@ -4,7 +4,7 @@ import TarjetaBase from './TarjetaBase.vue'
 const store = useCounterStore()
 </script>
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5 my-8">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 my-8">
     <tarjeta-base v-for="(juego, index) in store.listaJuegos" :key="index">
       <template #image>
         <img class="h-48 w-full object-cover" :src="juego.image" alt="Imagen del evento" />
