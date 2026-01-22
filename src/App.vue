06b82bfa7e1c5a48dc9c4b7b155657caaedc5090
Yaime <yaimeramil.camachole@elorrieta-errekamari.com>
2026-01-22 17:20:38 +0100

    feat: add Videojuegos view and FiltroVideojuegos component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import prueba from './views/Prueba.vue';
+import Videojuegos from './views/Videojuegos.vue';
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import prueba from './views/Prueba.vue';
   <h1 class="bg-emerald-500 text-center">GameFest!</h1>
   <!-- <RouterView /> -->
     <prueba />
+    <Videojuegos />
   </div>
 
 </template>
---
+++ src/components/Videojuegos/FiltroVideojuegos.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { reactive } from 'vue';
+const filtros = reactive({
+    titulo: '',
+    genero: '',
+    plataforma: ''
+});
+</script>
+
+<template>
+    <div class="bg-gray-800 p-4 rounded-lg shadow-lg mb-6 border border-gray-700">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+            <div>
+                <label class="block text-gray-400 text-sm mb-2">Buscar Juego</label>
+                <input v-model="filtros.titulo" type="text" placeholder="Ej: Super Mario..."
+                    class="w-full bg-gray-700 text-white rounded p-2 focus:outline-none focus:ring-2 focus:ring-emerald-500">
+            </div>
+        </div>
+    </div>
+</template>
