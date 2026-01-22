7a545cc44c945ca27c7625945bb38db6a7117b96
Yaime <yaimeramil.camachole@elorrieta-errekamari.com>
2026-01-22 20:45:38 +0100

    feat: update routing and add Eventos view with initial template

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,13 @@ import NavbarComponent from './components/NavbarComponent.vue';
 </script>
 
 <template>
-    <div>
-    <RouterView />
-  </div>
   <div id="app">
-  <NavbarComponent />
-</div>
-  <div class="min-h-screen bg-gray-900 text-white"> <NavBar />
-  <h1 class="bg-emerald-500 text-center">GameFest!</h1>
-  <!-- <RouterView /> -->
-    <prueba />
-    <Videojuegos />
+    <NavbarComponent />
+  </div>
+  <div>
+    <RouterView />
   </div>
 
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/vistaHome',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
