b4504c97c117e19c24e9dcc4d678db734ea92b0d
in2dw3 <in2dw3@elorrieta.int>
2026-01-22 16:01:40 +0100

    los cambios relizados

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="navbar" class="bg-green">
+    <div class="">
+      <img src="" alt="">
+    </div>
+    <div>
+        <i class="pi pi-user"></i>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
