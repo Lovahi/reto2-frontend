5aa9c4552d7b289be0e97d50cf8754fa561eeb1d
Younesse <younesse.khatibi@elorrieta-errekamari.com>
2026-01-23 15:51:36 +0100

    cambios en eventos.vue

--- src/views/Eventos.vue
+++ src/views/Eventos.vue
@@ -1,10 +1,23 @@
-<template>
-    <div>
-        <h1>Eventos</h1>
-    </div>
-</template>
-
 <script setup>
+import { useCounterStore } from '@/stores/counter'
+import TarjetaEvento from '@/components/Videojuegos/TarjetaEvento.vue'
+const store = useCounterStore()
 </script>
-
+<template>
+  <div class="flex flex-col sm:flex-row gap-5 py-4">
+    <select name="" id="" class="border-2 border-green-400 rounded-lg">
+      <option value="">--seleccione un tipo--</option>
+      <option value="" v-for="(tipo, index) in store.tiposEventos" :key="index">
+        {{ tipo }}
+      </option>
+    </select>
+    <input type="date" class="border-2 border-green-400 rounded-lg px-2"/>
+    <div>
+      <input type="checkbox" id="checkPlazas" /><label for="checkPlazas"
+        >Mostrar solo con plazas libres</label
+      >
+    </div>
+  </div>
+  <tarjeta-evento />
+</template>
 <style scoped></style>
