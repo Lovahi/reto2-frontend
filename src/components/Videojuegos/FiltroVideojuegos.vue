86b844294856b34c69a0c74207ff4dfc669fc5c4
Younesse <younesse.khatibi@elorrieta-errekamari.com>
2026-01-26 16:47:50 +0100

    prueba para filtrar

--- src/components/Videojuegos/FiltroVideojuegos.vue
+++ src/components/Videojuegos/FiltroVideojuegos.vue
@@ -1,21 +1,31 @@
 <script setup>
-import { reactive } from 'vue';
-const filtros = reactive({
-    titulo: '',
-    genero: '',
-    plataforma: ''
-});
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+const filtro = ref('')
+const filtrar = async () => {
+  try {
+    const respuesta = await fetch('URL_AQUI');
+    store.listaJuegos = await respuesta.json();
+  } catch (error) {
+    console.error('Error al obtener los datos:', error);
+  }
+};
 </script>
 
 <template>
-    <div class="bg-gray-800 p-4 rounded-lg shadow-lg mb-6 border border-gray-700">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-
-            <div>
-                <label class="block text-gray-400 text-sm mb-2">Buscar Juego</label>
-                <input v-model="filtros.titulo" type="text" placeholder="Ej: Super Mario..."
-                    class="w-full bg-gray-700 text-white rounded p-2 focus:outline-none focus:ring-2 focus:ring-emerald-500">
-            </div>
-        </div>
+  <div class="bg-gray-800 p-4 rounded-lg shadow-lg mb-6 border border-gray-700">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div>
+        <label class="block text-gray-400 text-sm mb-2">Buscar Juego</label>
+        <input
+          @keyup="filtrar"
+          v-model="filtro"
+          type="text"
+          placeholder="Ej: Super Mario..."
+          class="w-full bg-gray-700 text-white rounded p-2 focus:outline-none focus:ring-2 focus:ring-emerald-500"
+        />
+      </div>
     </div>
+  </div>
 </template>
