a832b1b5b1db9c88e52a7deccdb9ca7848a2eaca
younesseouaadouch <younesse.ouaadouch@elorrieta-errekamari.com>
2026-01-28 19:10:35 +0100

    style: enhance Login form layout and styling for better user experience

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
     
 
-<form class="max-w-sm mx-auto">
+<form class="max-w-sm  mx-auto text-[#0aabf6] mt-60 mb-20">
+  <div class="bg-[#000000] p-6 rounded-lg shadow-md">
   <div class="mb-5">
     <label for="email-alternative" class="block mb-2.5 text-sm font-medium text-heading">Your email</label>
     <input type="email" id="email-alternative" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow placeholder:text-body" placeholder="name@flowbite.com" required />
@@ -10,9 +11,12 @@
     <label for="password-alternative" class="block mb-2.5 text-sm font-medium text-heading">Your password</label>
     <input type="password" id="password-alternative" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow placeholder:text-body" placeholder="••••••••" required />
   </div>
-  <button type="submit" class="mx-[10px] p-[10px] text-center rounded-[5px] bg-[#4ab491] text-white hover:bg-[#3d9679] transition-colors">Iniciar sesión</button>
+  <div class="flex justify-center items-center mb-5">
+    <button type="submit" class="mx-[10px] p-[10px] text-center rounded-[5px] bg-[#0aabf6] text-white hover:bg-[#336176] transition-colors">Iniciar sesión</button>
+    </div>
   <div class="flex items-start mb-5">
-      <p class="ms-2 text-sm font-medium text-heading select-none">Aun no tienes cuenta?<a href="#" class="text-fg-brand hover:underline"> registrate</a>.</p>
+    <p class="ms-2 text-sm font-medium text-heading select-none">Aun no tienes cuenta?<a href="#" class="text-fg-brand text-white"> registrate</a>.</p>
+  </div>
   </div>
 </form>
 </template>
