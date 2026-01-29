89143347c0517365a87ba14f42dec784c672576c
younesseouaadouch <younesse.ouaadouch@elorrieta-errekamari.com>
2026-01-29 16:25:29 +0100

    feat: update routing and registration link in home view

--- src/views/Vistahome.vue
+++ src/views/Vistahome.vue
@@ -10,9 +10,9 @@
         <button class="mx-[10px] p-[10px] text-center rounded-[5px] text-[#4ab491] bg-white border border-[#4ab491] hover:bg-gray-50 transition-colors">
           Iniciar sesion
         </button>
-        <button class="mx-[10px] p-[10px] text-center rounded-[5px] bg-[#4ab491] text-white hover:bg-[#3d9679] transition-colors">
+        <RouterLink to="/registro" class="mx-[10px] p-[10px] text-center rounded-[5px] bg-[#4ab491] text-white hover:bg-[#3d9679] transition-colors">
           Registrarse
-        </button>
+        </RouterLink>
       </div>
   
   </div>
