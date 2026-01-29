8ef413e713bbf9e21e53206aa107b9cba6a6cb75
Yaimex <yaimeramil.camachole@elorrieta-errekamari.com>
2026-01-29 20:21:43 +0100

    OKy

--- src/views/Vistahome.vue
+++ src/views/Vistahome.vue
@@ -3,14 +3,14 @@
       
       <div class="flex flex-col gap-[30px] text-center text-[xx-large]">
         <h1 class="text-[#4ab491] font-bold">GameFest!</h1>
-        <p class="text-gray-600">Explora el mundo virtual!</p>
+        <p class="text-gray-600"></p>
       </div>
   
-      <div class="mt-5">
-        <button class="mx-[10px] p-[10px] text-center rounded-[5px] text-[#4ab491] bg-white border border-[#4ab491] hover:bg-gray-50 transition-colors">
+      <div class="mt-90">
+        <button class="mx-[10px] p-[10px] text-center rounded text-[#4ab491] bg-white border border-[#4ab491] hover:bg-gray-50 transition-colors">
           Iniciar sesion
         </button>
-        <button class="mx-[10px] p-[10px] text-center rounded-[5px] bg-[#4ab491] text-white hover:bg-[#3d9679] transition-colors">
+        <button class="mx-[10px] p-[10px] text-center rounded bg-[#4ab491] text-white hover:bg-[#3d9679] transition-colors">
           Registrarse
         </button>
       </div>
