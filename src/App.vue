678bfc40e78951279165efe4311edc50c7b77bad
Lovahi <74183797+Lovahi@users.noreply.github.com>
2026-02-03 18:07:42 +0100

    feat: update css

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import FooterComponent from './components/FooterComponent.vue'
 </script>
 
 <template>
-  <div id="app" class="min-h-screen flex flex-col text-black font-sans">
+  <div id="app" class="min-h-screen flex flex-col font-sans">
     <NavbarComponent />
 
     <main class="flex-1 relative">
